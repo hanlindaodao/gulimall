bd68559ebcdd84ed693092b9fd91badbd53f5735
dao <601760700@qq.com>
2024-08-24 01:53:19 +0800

    将每个服务加入到nacos中

--- gulimall-coupon/src/main/java/com/daodao/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/daodao/gulimall/coupon/GulimallCouponApplication.java
@@ -1,10 +1,10 @@
 package com.daodao.gulimall.coupon;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@MapperScan("com.daodao.gulimall.coupon.dao")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallCouponApplication {
 
