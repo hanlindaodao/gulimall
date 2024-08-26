a43a6c350724c94d306d54d7ee96d68fb71eea90
dao <601760700@qq.com>
2024-08-27 01:10:22 +0800

    使用renrenfast项目前后端开发进行联调 增加跨域，这里没懂

--- gulimall-product/src/main/java/com/daodao/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/daodao/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,13 @@
 package com.daodao.gulimall.product.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -16,6 +22,9 @@ import com.daodao.gulimall.product.service.CategoryService;
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
 
+//    @Autowired
+//    CategoryDao categoryDao;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<CategoryEntity> page = this.page(
@@ -24,6 +33,45 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        // 1.查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        // 2.组装成父子的树形结构
+        // 2.1. 找到所有的一级分类
+        // 父分类id为零，说明为一级分类
+        List<CategoryEntity> level1Menus = entities.stream()
+                .filter(categoryEntity -> categoryEntity.getParentCid() == 0)
+                .map((menu) -> {
+                    menu.setChildren(getChildrens(menu, entities));
+                    return menu;
+                }).sorted((menu1, menu2) -> {
+                    return (menu1.getSort() == null ? 0 : menu1.getSort())
+                            - (menu2.getSort() == null ? 0 : menu2.getSort());
+                }).collect(Collectors.toList());
+
+        return level1Menus;
+    }
+
+    // 递归查找所有菜单的子菜单
+    private List<CategoryEntity> getChildrens(CategoryEntity root, List<CategoryEntity> all) {
+
+        List<CategoryEntity> childrens = all.stream()
+                .filter(categoryEntity -> categoryEntity.getParentCid() == root.getCatId())
+                .map(categoryEntity -> {
+                    // 找到子菜单
+                    categoryEntity.setChildren(getChildrens(categoryEntity, all));
+                    return categoryEntity;
+                })
+                // 排序
+                .sorted((menu1, menu2) ->
+                        (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort()))
+                .collect(Collectors.toList());
+
+        return childrens;
     }
 
 }
