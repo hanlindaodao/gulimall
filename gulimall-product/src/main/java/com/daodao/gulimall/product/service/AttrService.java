d9da9607afeaef63834505e64e345d3b9ff5daec
dao <601760700@qq.com>
2024-09-11 08:28:20 +0800

    - 增加VO，增加属性分组和属性的联合增加

--- gulimall-product/src/main/java/com/daodao/gulimall/product/service/AttrService.java
+++ gulimall-product/src/main/java/com/daodao/gulimall/product/service/AttrService.java
@@ -3,6 +3,7 @@ package com.daodao.gulimall.product.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.daodao.common.utils.PageUtils;
 import com.daodao.gulimall.product.entity.AttrEntity;
+import com.daodao.gulimall.product.vo.AttrVo;
 
 import java.util.Map;
 
@@ -16,5 +17,7 @@ import java.util.Map;
 public interface AttrService extends IService<AttrEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    void saveAttr(AttrVo attr);
 }
 
--- gulimall-product/src/main/java/com/daodao/gulimall/product/service/impl/AttrServiceImpl.java
+++ gulimall-product/src/main/java/com/daodao/gulimall/product/service/impl/AttrServiceImpl.java
@@ -1,5 +1,10 @@
 package com.daodao.gulimall.product.service.impl;
 
+import com.daodao.gulimall.product.dao.AttrAttrgroupRelationDao;
+import com.daodao.gulimall.product.entity.AttrAttrgroupRelationEntity;
+import com.daodao.gulimall.product.vo.AttrVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -16,6 +21,9 @@ import com.daodao.gulimall.product.service.AttrService;
 @Service("attrService")
 public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements AttrService {
 
+    @Autowired
+    AttrAttrgroupRelationDao attrAttrgroupRelationDao;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<AttrEntity> page = this.page(
@@ -24,6 +32,20 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public void saveAttr(AttrVo attr) {
+        AttrEntity attrEntity = new AttrEntity();
+        BeanUtils.copyProperties(attr, attrEntity);
+        //1、保存基本属性
+        this.save(attrEntity);
+
+        //2、保存关联关系
+        AttrAttrgroupRelationEntity attrAttrgroupRelationEntity = new AttrAttrgroupRelationEntity();
+        attrAttrgroupRelationEntity.setAttrGroupId(attr.getAttrGroupId());
+        attrAttrgroupRelationEntity.setAttrId(attrEntity.getAttrId());
+        attrAttrgroupRelationDao.insert(attrAttrgroupRelationEntity);
     }
 
 }
